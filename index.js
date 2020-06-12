b0f475abb54b32f46698407eaa07f3f9a0ad3b7f
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-06-12 11:58:04 -0400

    task 5 done

--- index.js
+++ index.js
@@ -266,10 +266,16 @@ get20s(artists);
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
-  }
+function removeArtist(array, index) {
+
+  array.splice(index, 1);
+  console.log(array.length);
+  return array;
+
+}
   
+// removeArtist(artists, 9);
+
   /**
 
 
